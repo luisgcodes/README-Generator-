511a923dcef44b9583151e06e0242dc9fad2d862
luisgcodes <lcienfuegos95@gmail.com>
2022-04-12 20:37:27 -0700

    added 1st input

--- Develop/index.js
+++ Develop/index.js
@@ -4,8 +4,18 @@ const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [{
-    type: "input"
-
+    console.log(questions)
+    type: "input",
+    name: "Project Title"
+    message: "What is your project's title?",
+    validate: projectInput => {
+    if(projectInput){
+        return true;
+    }else{
+        console.log("Please enter your project title");
+        return false;
+      }
+    }
 }];
 
 // TODO: Create a function to write README file
