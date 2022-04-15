89cf6b677809f2a6f0186c8251914ca1f7bfcd59
luisgcodes <lcienfuegos95@gmail.com>
2022-04-14 18:02:52 -0700

    finished adding inputs

--- Develop/index.js
+++ Develop/index.js
@@ -1,33 +1,46 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const Choices = require('inquirer/lib/objects/choices');
+
 
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
     name: "Project Title",
     message: "What is your project's title?",
-    validate: projectInput => {
-    if(projectInput){
-        return true;
-    }else{
-        console.log("Please enter your project title");
-        return false;
-      }
-    }
 },
 {
     type: 'input',
     name: 'Description',
-    message: 'Provide a short description of your project',
-    validate: descripInput => {
-        if(descripInput){
-            return true;
-        }else{
-            console.log("Please enter your project's description");
-            return false;
-        }
-    }
+    message: 'Provide a short description of your project.',
+},
+{
+    type: 'input',
+    name: 'Table of Contents',
+    message: 'Provide a table of contents.',
+},
+{
+    type: 'input',
+    name: 'Installation',
+    message: 'Provide steps required to install your project.',
+},
+{
+    type: 'input',
+    name: 'Usage',
+    message: 'Provide instructions and examples for use.',
+},
+{
+    type: 'input',
+    name: 'Credits',
+    message: 'Provide a list of your contributors, if any.'
+},
+{
+    type: 'input',
+    name: 'License',
+    message: 'Choose a license',
+    Choices: ['GPL', 'MIT', 'Apache', 'MS-PL', 'No license'],
 },
 
 ];
